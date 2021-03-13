8029920130c012b4ffdf587740ac8c451b14628d
andy941 <finocchio.andrea94@gmail.com>
2021-03-13 13:31:46 +0000

    applet startups fixes

--- SecondScreen.sh
+++ SecondScreen.sh
@@ -3,6 +3,7 @@ xrandr --auto
 xrandr --output eDP1 --primary
 xrandr | grep "HDMI1 connected" && xrandr --output HDMI1 --primary --left-of eDP1
 xrandr | grep "^DP1 connected" && xrandr --output DP1 --primary --left-of eDP1
-nitrogen --restore
+
+
 
 
