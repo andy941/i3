a846b38e18102ad88e077817ba571dc87ac00be5
andy941 <finocchio.andrea94@gmail.com>
2021-09-11 15:52:10 +0100

    small stuff

--- SecondScreen.sh
+++ SecondScreen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 xrandr --auto
-xrandr --output eDP1 --primary
-xrandr | grep "HDMI1 connected" && xrandr --output HDMI1 --primary --left-of eDP1
-xrandr | grep "^DP1 connected" && xrandr --output DP1 --primary --left-of eDP1
+xrandr --output eDP-1 --primary
+xrandr | grep "^DP-1 connected" && xrandr --output DP-1 --primary --left-of eDP-1
+xrandr | grep "^DP-2 connected" && xrandr --output DP-2 --primary --left-of eDP-1
 
 
 
