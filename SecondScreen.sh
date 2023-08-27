d94718ad5bf190b9f96de55afc19dc7f1ef012e4
andy941 <finocchio.andrea94@gmail.com>
2023-08-27 23:36:28 +0100

    Adjust small things for second 4K monitor

--- SecondScreen.sh
+++ SecondScreen.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+
 xrandr --auto
 xrandr --output eDP-1 --primary
 xrandr | grep "^DP-1 connected" && xrandr --output DP-1 --primary --left-of eDP-1
 xrandr | grep "^DP-2 connected" && xrandr --output DP-2 --primary --left-of eDP-1
-
-
-
-
+xrandr | grep "^DP-1-1 connected" && xrandr --output DP-1-1 --primary --left-of eDP-1 --mode 3840x2160
+xrandr | grep "^DP-2-2 connected" && xrandr --output DP-2-2 --primary --left-of eDP-1 --mode 3840x2160
--- polybar_launcher.sh
+++ polybar_launcher.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 #polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 killall -q polybar
+sleep 2
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar -r -l error --config=$HOME/.config/polybar/config.ini main 2>&1 | tee -a /tmp/polybar1.log & disown
+m_primary=$(polybar --list-monitors | grep "primary" | cut -d":" -f1)
+MONITOR=$m_primary polybar --reload main &
 
-echo "Bars launched..."
+for m in $(polybar --list-monitors | grep -v "primary" | cut -d":" -f1); do
+    MONITOR=$m polybar --reload main &
+done
