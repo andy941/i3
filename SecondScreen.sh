d50566101d0460641a718596bd04084cad743002
andy941 <finocchio.andrea94@gmail.com>
2024-01-13 19:40:51 +0000

    Second Screen fix and dunst

--- SecondScreen.sh
+++ SecondScreen.sh
@@ -2,7 +2,7 @@
 
 xrandr --auto
 xrandr --output eDP-1 --primary
-xrandr | grep "^DP-1 connected" && xrandr --output DP-1 --primary --left-of eDP-1
-xrandr | grep "^DP-2 connected" && xrandr --output DP-2 --primary --left-of eDP-1
-xrandr | grep "^DP-1-1 connected" && xrandr --output DP-1-1 --primary --left-of eDP-1 --mode 3840x2160
-xrandr | grep "^DP-2-2 connected" && xrandr --output DP-2-2 --primary --left-of eDP-1 --mode 3840x2160
+xrandr | grep "^DP-1 connected" && xrandr --output DP-1 --primary --above eDP-1
+xrandr | grep "^DP-2 connected" && xrandr --output DP-2 --primary --above eDP-1
+xrandr | grep "^DP-1-1 connected" && xrandr --output DP-1-1 --primary --above eDP-1 --mode 3840x2160
+xrandr | grep "^DP-2-2 connected" && xrandr --output DP-2-2 --primary --above eDP-1 --mode 3840x2160
